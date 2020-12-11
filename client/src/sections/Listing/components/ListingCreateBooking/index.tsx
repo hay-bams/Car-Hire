7081c5df8177654d5c63637c3af891c019b7f70a
hay-bams <purposenigeria@gmail.com>
2020-12-11 18:16:50 +0100

    verify checkin and checkout date

---
+++ client/src/sections/Listing/components/ListingCreateBooking/index.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { Typography, Divider, DatePicker, Button } from 'antd';
+import { Listing } from '../../../../lib/graphql/queries/Listing/__generated__/Listing';
+import moment, { Moment } from 'moment';
+import { displayErrorMessage } from '../../../../lib/utils';
+
+interface Props {
+  listing: Listing['listing'];
+}
+
+const { Paragraph, Title, Text } = Typography;
+
+const bookingFieldTitle = {
+  color: '#477b71',
+  margin: '20px auto 0 auto',
+  width: '40%',
+};
+
+export const ListingCreateBooking = ({ listing }: Props) => {
+  const [checkin, setCheckin] = useState<Moment | null>(null);
+  const [checkout, setCheckout] = useState<Moment | null>(null);
+
+  const disabledDate = (currentDate: Moment) => {
+    if (currentDate) {
+      return currentDate.isBefore(moment().endOf('days'));
+    } else {
+      return false;
+    }
+  };
+
+  const disableCheckout = !checkin;
+  const disableButton =
+    !checkin || !checkout || (checkout && checkout.isBefore(checkin));
+
+  const verifyAndSetCheckoutDate = (date: Moment | null) => {
+    setCheckout(date);
+
+    if (date && date.isBefore(checkin)) {
+      displayErrorMessage('Checkout date cannot be before checkin');
+    }
+  };
+
+  const verifyAndSetCheckindate = (date: Moment | null) => {
+    setCheckin(date);
+
+    if (date && date.isAfter(checkout)) {
+      displayErrorMessage('Checkout date cannot be before checkin');
+    }
+  };
+
+  return (
+    <Paragraph
+      className="booking_column"
+      style={{ width: '90%', margin: '0 auto' }}
+    >
+      <Title level={2} style={bookingFieldTitle}>
+        ${listing.price}
+        <Text style={{ color: '#676a6c' }}>/day</Text>
+      </Title>
+      <Divider style={{ backgroundColor: '#e8e8e8' }} />
+      <Title
+        level={5}
+        style={{ margin: '0 auto', width: '20%', color: '#676a6c' }}
+      >
+        Check in
+      </Title>
+      <Paragraph style={{ width: '50%', margin: '20px auto' }}>
+        <DatePicker
+          onChange={(date: Moment | null) => verifyAndSetCheckindate(date)}
+          disabledDate={disabledDate}
+          style={{ width: '100%' }}
+        />
+      </Paragraph>
+
+      <Title
+        level={5}
+        style={{ margin: '0  auto', width: '20%', color: '#676a6c' }}
+      >
+        Check out
+      </Title>
+      <Paragraph style={{ width: '50%', margin: '20px auto 50px auto' }}>
+        <DatePicker
+          onChange={(date: Moment | null) => verifyAndSetCheckoutDate(date)}
+          disabledDate={disabledDate}
+          disabled={disableCheckout}
+          style={{ width: '100%' }}
+        />
+      </Paragraph>
+
+      <Divider style={{ backgroundColor: '#e8e8e8' }} />
+      <Paragraph style={{ width: '40%', margin: '20px auto 50px auto' }}>
+        <Button disabled={disableButton} style={{ width: '100%' }}>
+          Request to book
+        </Button>
+      </Paragraph>
+    </Paragraph>
+  );
+};
--- client/src/sections/Listing/index.tsx
+++ client/src/sections/Listing/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Row, Col, Typography, Divider, DatePicker, Space, Button } from 'antd';
-import { Layout } from 'antd';
+import { Row, Col, Layout } from 'antd';
 import { RouteComponentProps } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { LISTING } from '../../lib/graphql/queries/Listing';
@@ -8,20 +7,13 @@ import {
   ListingVariables,
   Listing as ListingData,
 } from '../../lib/graphql/queries/Listing/__generated__/Listing';
-import { ListingDetails } from './components';
+import { ListingCreateBooking, ListingDetails } from './components';
 
 interface MatchParams {
   id: string;
 }
 
 const { Content } = Layout;
-const { Paragraph, Title, Text } = Typography;
-
-const bookingFieldTitle = {
-  color: '#477b71',
-  margin: '20px auto 0 auto',
-  width: '40%',
-};
 
 export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
   const { data, loading, error } = useQuery<ListingData, ListingVariables>(
@@ -42,39 +34,10 @@ export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
   const ListingDetail =
     data && data.listing ? <ListingDetails listing={data.listing} /> : null;
 
-  const BookingField = (
-    <Paragraph className="booking_column" style={{ width: '90%', margin: '0 auto' }}>
-      <Title level={2} style={bookingFieldTitle}>
-        ${data?.listing.price}
-        <Text style={{ color: '#676a6c' }}>/day</Text>
-      </Title>
-      <Divider style={{ backgroundColor: '#e8e8e8' }} />
-      <Title
-        level={5}
-        style={{ margin: '0 auto', width: '20%', color: '#676a6c' }}
-      >
-        Check in
-      </Title>
-      <Paragraph style={{ width: '50%', margin: '20px auto' }}>
-        <DatePicker style={{ width: '100%' }} />
-      </Paragraph>
-
-      <Title
-        level={5}
-        style={{ margin: '0  auto', width: '20%', color: '#676a6c' }}
-      >
-        Check out
-      </Title>
-      <Paragraph style={{ width: '50%', margin: '20px auto 50px auto' }}>
-        <DatePicker style={{ width: '100%' }} />
-      </Paragraph>
-
-      <Divider style={{ backgroundColor: '#e8e8e8' }} />
-      <Paragraph style={{ width: '40%', margin: '20px auto 50px auto' }}>
-        <Button style={{ width: '100%' }}>Request to book</Button>
-      </Paragraph>
-    </Paragraph>
-  );
+  const BookingField =
+    data && data.listing ? (
+      <ListingCreateBooking listing={data.listing} />
+    ) : null;
 
   return (
     <Content className="listing-details-content-area">
@@ -82,7 +45,11 @@ export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
         <Col className="gutter-row" lg={14}>
           {ListingDetail}
         </Col>
-        <Col className="gutter-row" lg={8} style={{ border: '2px solid #e8e8e8', height: '500px' }}>
+        <Col
+          className="gutter-row"
+          lg={8}
+          style={{ border: '2px solid #e8e8e8', height: '500px' }}
+        >
           {BookingField}
         </Col>
       </Row>
